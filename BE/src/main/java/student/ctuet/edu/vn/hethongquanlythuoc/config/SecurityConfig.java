eca0dbe7e25bff860ba8e97d410a1bf705b92665
Tin Dao <trungtin07012005@gmail.com>
2026-05-16 12:55:45 +0700

    fix cors

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:5173"));
+                config.setAllowedOrigins(List.of("http://localhost:5173", "https://he-thong-quan-ly-thuoc-ctut.vercel.app"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
