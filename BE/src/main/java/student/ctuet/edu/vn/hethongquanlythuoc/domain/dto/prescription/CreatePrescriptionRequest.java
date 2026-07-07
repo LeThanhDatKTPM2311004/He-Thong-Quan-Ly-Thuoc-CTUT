efe4a10e78797a69d882e121ef4733eff4bd7e62
Tin Dao <trungtin07012005@gmail.com>
2026-07-07 16:43:50 +0700

    feat(Prescription): add note into resquest and response

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/dto/prescription/CreatePrescriptionRequest.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/dto/prescription/CreatePrescriptionRequest.java
@@ -3,13 +3,14 @@ package student.ctuet.edu.vn.hethongquanlythuoc.domain.dto.prescription;
 import java.util.List;
 
 public record CreatePrescriptionRequest(
-        String studentCode,
-        String diagnosis,
-        String note,
-        String medicalStaff,
-        List<DetailItem> details) {
-    public record DetailItem(
-            long medicineId,
-            int quantity) {
-    }
+                String studentCode,
+                String diagnosis,
+                String note,
+                String medicalStaff,
+                List<DetailItem> details) {
+        public record DetailItem(
+                        long medicineId,
+                        int quantity,
+                        String note) {
+        }
 }
