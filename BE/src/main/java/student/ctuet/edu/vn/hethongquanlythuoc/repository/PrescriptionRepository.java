92e84c6c989b05efa234374b049ba7a5e870716d
Tin Dao <trungtin07012005@gmail.com>
2026-07-07 17:03:05 +0700

    feat(prescription): add note for create, update, and maptoresponse

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/repository/PrescriptionRepository.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/repository/PrescriptionRepository.java
@@ -9,13 +9,14 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import student.ctuet.edu.vn.hethongquanlythuoc.domain.Prescription;
 
 public interface PrescriptionRepository extends JpaRepository<Prescription, String>,
         JpaSpecificationExecutor<Prescription> {
 
-    @Query("SELECT COUNT(p) FROM Prescription p WHERE p.prescriptionCode LIKE :prefix%")
-    long countByCodePrefix(String prefix);
+    @Query("SELECT COUNT(p) FROM Prescription p WHERE p.prescriptionCode LIKE CONCAT(:prefix, '%')")
+    long countByCodePrefix(@Param("prefix") String prefix);
 
     Page<Prescription> findAll(Specification<Prescription> spec, Pageable pageable);
 
